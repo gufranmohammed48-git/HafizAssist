386b7eed8539dde9814610f45cec8b745a1d0fbd
gufranmohammed48-git <gufranmohammed48@gmail.com>
2026-09-20 11:25:58 -0400

    moved page navigation button
    moved page navigation button

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Increment with every delivered change; keep index.html's badge in sync.
-const VERSION = 'V14';
+const VERSION = 'V15';
 const PREFIX = `hafizassist-shell:${self.registration.scope}:`;
 const SHELL = PREFIX + VERSION;
 const url = path => new URL(path, self.registration.scope).href;
